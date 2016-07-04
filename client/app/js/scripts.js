a30ed077f2f4d5c5d8e3eccf89b9eeff2d0bf741
Tania Blindaruk <t.blindaruk@gmail.com>
2016-07-04 22:04:18 +0300

    15. Добавити гугл аналітику

--- client/app/js/scripts.js
+++ client/app/js/scripts.js
@@ -28,3 +28,14 @@ function readCookie(name) {
     }
     return '';
 }
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
